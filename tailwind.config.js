ba7d0231c2abb25c46ec217a3389d334ae4b7679
RabinThapa1998 <imrabin1998@gmail.com>
2022-09-28 23:19:39 +0545

    🩹 FIx: tailwind css config issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  corePlugins: {
+    preflight: false,
+  },
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
