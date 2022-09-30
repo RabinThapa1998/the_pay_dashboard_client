a9b1b48f632066fe10f2d26845e5f6e6d94e6191
RabinThapa1998 <imrabin1998@gmail.com>
2022-09-30 22:38:44 +0545

    ✨ Feat Add Program Detail API query

--- src/containers/programs/ProgramsDetail.tsx
+++ src/containers/programs/ProgramsDetail.tsx
@@ -5,7 +5,37 @@ import { Modal } from '~/components/common';
 import { useQuery } from '@tanstack/react-query';
 import { request } from '~components/util';
 import { Space, Spin } from 'antd';
+import { useParams } from 'react-router-dom';
 
 export function ProgramsDetail() {
-  return <Row gutter={[16, 16]}>detail</Row>;
+  const { id } = useParams();
+  console.log('🚀 ~ file: ProgramsDetail.tsx ~ line 12 ~ ProgramsDetail ~ id', id);
+  const { data: res, isLoading } = useQuery(
+    ['programs', id],
+    (): Promise<{
+      data: {
+        contestents: { [key: string]: string }[];
+        programs: any;
+      };
+    }> =>
+      request({
+        url: `/programs/${id}`,
+        method: 'GET',
+      }),
+  );
+
+  //   console.log('🚀 ~ file: ProgramsDetail.tsx ~ line 13 ~ ProgramsDetail ~ data', data);
+  return (
+    <Row gutter={[16, 16]}>
+      <Col span={24}>
+        {res ? (
+          res.data.contestents.map((item) => <p key={item.id}>{item.email}</p>)
+        ) : (
+          <Space direction='horizontal' style={{ width: '100%', justifyContent: 'center' }}>
+            <Spin size='large' />
+          </Space>
+        )}
+      </Col>
+    </Row>
+  );
 }
