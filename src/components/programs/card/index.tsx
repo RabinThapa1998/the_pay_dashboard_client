4c352a0fa44399358567f4745aa0d40c41b19a1d
RabinThapa1998 <imrabin1998@gmail.com>
2022-10-06 22:05:24 +0545

    ✨ Feat: Add Programs Delete

--- src/components/programs/card/index.tsx
+++ src/components/programs/card/index.tsx
@@ -1,7 +1,9 @@
 import { EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import { useMutation } from '@tanstack/react-query';
 import { Avatar, Card, CardProps, Button } from 'antd';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { request } from '~/components/util';
 import { Modal } from '~components/common';
 
 const { Meta } = Card;
@@ -13,6 +15,21 @@ interface ICard extends CardProps {
 }
 function CardComponent({ title, desc, programId, ...rest }: ICard) {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const { mutate } = useMutation(
+    () =>
+      request({
+        url: `/programs/${programId}`,
+        method: 'DELETE',
+      }),
+    {
+      onSuccess: () => {
+        setIsModalOpen(false);
+      },
+      onError: () => {
+        setIsModalOpen(false);
+      },
+    },
+  );
 
   const navigate = useNavigate();
   const handleClickMore = () => {
@@ -29,6 +46,9 @@ function CardComponent({ title, desc, programId, ...rest }: ICard) {
   };
   const hanldeModalCancel = () => {
     setIsModalOpen(false);
+  };
+  const handleProgramDelete = async () => {
+    await mutate();
   };
   return (
     <Card
@@ -53,8 +73,24 @@ function CardComponent({ title, desc, programId, ...rest }: ICard) {
         title={title}
         description={desc}
       />
-      <Modal open={isModalOpen} onOk={handleModalOk} onCancel={hanldeModalCancel}>
-        {<p>Setting</p>}
+      <Modal open={isModalOpen} onOk={handleModalOk} onCancel={hanldeModalCancel} footer={null}>
+        {
+          <div className='flex flex-col items-center justify-center'>
+            <p className='text-lg'>{title}</p>
+            <h1 className='text-base font-semibold'>
+              Are you sure you want to delete this program?
+            </h1>
+            <p className='text-sm text-gray-500 text-center'>
+              All the contestant related to that programs will be deleted along with the program.
+            </p>
+            <div className='flex flex-row items-center justify-end w-full gap-2'>
+              <Button type='primary' onClick={handleProgramDelete} danger>
+                Delete
+              </Button>
+              <Button onClick={hanldeModalCancel}>No</Button>
+            </div>
+          </div>
+        }
       </Modal>
     </Card>
   );
