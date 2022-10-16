79802d809f334115959ef7b4ddada8f9dace4c72
RabinThapa1998 <imrabin1998@gmail.com>
2022-10-16 10:34:22 +0545

    ✨ : Feat: Add Payment Schema Feature

--- src/components/programs/form-component/ProgramAddForm.tsx
+++ src/components/programs/form-component/ProgramAddForm.tsx
@@ -5,8 +5,12 @@ import { useMutation } from '@tanstack/react-query';
 const { TextArea } = Input;
 
 const ProgramAddForm: React.FC<{ closeModal: () => void }> = ({ closeModal }) => {
+  const [form] = Form.useForm();
   const [fieldCount, setFieldCount] = useState([0]);
-  const [votesCost, setVotesCost] = useState({});
+  const [votesCost, setVotesCost] = useState<any>({
+    votes: 0,
+    cost: 0,
+  });
   const [votesCostField, setVotesCostField] = useState<any>([]);
   console.log('🚀 ~ file: ProgramAddForm.tsx ~ line 11 ~ votesCostField', votesCostField);
   const { mutate, isLoading } = useMutation(
@@ -28,24 +32,20 @@ const ProgramAddForm: React.FC<{ closeModal: () => void }> = ({ closeModal }) =>
     },
   );
 
-  const handleVotesChange = (value: any) => {
-    console.log('votes', value);
+  const handleCostVotesChange = (value: any, name: string) => {
     setVotesCost({
       ...votesCost,
-      votes: value,
+      [name]: value,
     });
   };
-  const handleCostsChange = (value: any) => {
-    console.log('cost', value);
-    setVotesCost({
-      ...votesCost,
-      cost: value,
-    });
+  const handleVotesCostField = () => {
+    if (votesCost.cost !== 0 && votesCost.votes !== 0) {
+      setVotesCostField([...votesCostField, votesCost]);
+    }
   };
-
   const onFinish = async (values: any) => {
     console.log('🚀 ~ file: ProgramAddForm.tsx ~ line 28 ~ onFinish ~ values', values);
-    // await mutate(values);
+    await mutate({ ...values, payment_schema: votesCostField });
   };
 
   const onFinishFailed = (errorInfo: any) => {
@@ -53,9 +53,10 @@ const ProgramAddForm: React.FC<{ closeModal: () => void }> = ({ closeModal }) =>
   };
   const handleVotesCostAdd = (i: number) => {
     setFieldCount([...fieldCount, i]);
-
-    setVotesCostField([...votesCostField, votesCost]);
-    setVotesCost({});
+    setVotesCost({
+      votes: 0,
+      cost: 0,
+    });
   };
   const handleVotesCostDelete = (i: number) => {
     const newFieldCount = fieldCount.filter((item) => item !== i);
@@ -72,6 +73,7 @@ const ProgramAddForm: React.FC<{ closeModal: () => void }> = ({ closeModal }) =>
       onFinishFailed={onFinishFailed}
       autoComplete='off'
       layout='vertical'
+      form={form}
     >
       <Form.Item
         label='Progam Name'
@@ -80,20 +82,27 @@ const ProgramAddForm: React.FC<{ closeModal: () => void }> = ({ closeModal }) =>
       >
         <Input />
       </Form.Item>
-
       <Form.Item label='Description' name='desc' rules={[{ required: false }]}>
         <TextArea rows={4} />
       </Form.Item>
-
       {fieldCount.map((item) => (
         <Input.Group compact key={item}>
-          <InputNumber name='votes' onChange={handleVotesChange} type='number' />
-          <InputNumber name='cost' onChange={handleCostsChange} type='number' />
+          <InputNumber
+            name='votes'
+            onChange={(value) => handleCostVotesChange(value, 'votes')}
+            onBlur={handleVotesCostField}
+            type='number'
+          />
+          <InputNumber
+            name='cost'
+            onChange={(value) => handleCostVotesChange(value, 'cost')}
+            onBlur={handleVotesCostField}
+            type='number'
+          />
           <Button onClick={() => handleVotesCostAdd(item + 1)}>Add</Button>
           {item > 0 && <Button onClick={() => handleVotesCostDelete(item)}>Delete</Button>}
         </Input.Group>
       ))}
-
       <Form.Item>
         <Button type='primary' htmlType='submit' loading={isLoading}>
           Submit
